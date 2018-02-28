0c56265b4797760870d206a0d9c62cb1fbdc58de
prathv <prathveer.r@gmail.com>
2018-02-27 20:19:31 -0800

    Initial commit

---
+++ app/src/main/java/com/example/prath/notekeeper/NoteListActivity.java
@@ -0,0 +1,61 @@
+package com.example.prath.notekeeper;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.List;
+
+public class NoteListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note_list);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        initalizeDisplayContent();
+    }
+
+    private void initalizeDisplayContent() {
+
+        final ListView listView = (ListView) findViewById(R.id.list_notes);
+
+        List<NoteInfo> notes = DataManager.getInstance().getNotes();
+
+        final ArrayAdapter<NoteInfo> adapterNotes = new ArrayAdapter<NoteInfo>(this, android.R.layout.simple_list_item_1,notes);
+
+        listView.setAdapter(adapterNotes);
+
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(NoteListActivity.this,NoteActivity.class);
+                NoteInfo note = (NoteInfo) adapterNotes.getItem(i);
+                intent.putExtra(NoteActivity.NOTE_INFO,note);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+
+}
